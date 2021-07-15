6170c2e2632820c4eb08f9460ab71b8e2704334b
rigole <foplacide@gmail.com>
2021-07-15 21:41:48 +0100

    play pause functionality implemented

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const currentTime = document.querySelector('.time-elapsed');
+const volumeBar = document.querySelector('.volume-bar');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
-
+function togglePlay() {
+    if (video.paused){
+        video.play();
+    }
+    else {
+        video.pause();
+    }
+}
 
 // Progress Bar ---------------------------------- //
 
@@ -18,3 +34,8 @@
 
 // Fullscreen ------------------------------- //
 
+
+// Event Listeners
+
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
